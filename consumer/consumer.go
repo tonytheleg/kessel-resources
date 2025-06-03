ae7fecaac9cbe37931b9c501e0eb4dc8de58e1c0
Jonathan Marcantonio <jmarcant@redhat.com>
2025-06-03 15:05:52 -0400

    init consumer testing
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

---
+++ consumer/consumer.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	kessel "github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta1/resources"
+	"github.com/project-kessel/inventory-client-go/common"
+	"github.com/project-kessel/inventory-client-go/v1beta1"
+)
+
+func main() {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		// "sasl.username":     "inventory-consumer",
+		// "sasl.password":     "",
+
+		// "security.protocol": "sasl_plaintext",
+		// "sasl.mechanisms":   "SCRAM-SHA-512",
+		"group.id":          "inventory-consumer",
+		"auto.offset.reset": "earliest"})
+
+	if err != nil {
+		fmt.Printf("failed to create consumer: %v", err)
+		os.Exit(1)
+	}
+
+	topic := "outbox.event.kessel.tuples" //todo:change
+	err = c.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		fmt.Printf("failed to subscribe to topic: %v", err)
+		os.Exit(1)
+	}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	client, err := v1beta1.New(common.NewConfig(
+		common.WithgRPCUrl("localhost:9081"),
+		common.WithTLSInsecure(true),
+		common.WithAuthEnabled("", "", ""),
+	))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	opts, err := client.GetTokenCallOption()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Process messages
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev, err := c.ReadMessage(100 * time.Millisecond)
+			if err == nil {
+				fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
+					*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+
+				// figure out what request to send to inventory.
+				request := kessel.CreateRhelHostRequest{RhelHost: &kessel.RhelHost{
+					Metadata: &kessel.Metadata{
+						ResourceType: "rhel-host",
+						WorkspaceId:  "",
+					},
+					ReporterData: &kessel.ReporterData{
+						ReporterType:       kessel.ReporterData_ACM,
+						ReporterInstanceId: "service-account-svc-test",
+						ConsoleHref:        "www.example.com",
+						ApiHref:            "www.example.com",
+						LocalResourceId:    "1",
+						ReporterVersion:    "0.1",
+					},
+				}}
+
+				_, err = client.RhelHostServiceClient.CreateRhelHost(context.Background(), &request, opts...)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+			} else if !err.(kafka.Error).IsTimeout() {
+				fmt.Printf("Consumer error: %v (%v)\n", err, ev)
+			}
+		}
+	}
+
+	c.Close()
+}
---
+++ consumer/go.mod
@@ -0,0 +1,33 @@
+module generic-consumer
+
+go 1.24.2
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.10.0
+
+require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.6-20250423154025-ec0af4932e5e.1 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	github.com/authzed/grpcutil v0.0.0-20250221190651-1985b19b35b8 // indirect
+	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-kratos/kratos/v2 v2.8.4 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/project-kessel/inventory-api v0.0.0-20250507142859-a4225659a5b2 // indirect
+	github.com/project-kessel/inventory-client-go v0.0.0-20250507151904-9921f1bf1e8e
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250422160041-2d3770c4ea7f // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250422160041-2d3770c4ea7f // indirect
+	google.golang.org/grpc v1.72.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
